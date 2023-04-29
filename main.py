1d6c77a0ea3f6bf40e8b6c4233387c921170a365
Tri Chau <trichauc2c@gmail.com>
2023-04-29 13:24:01 -0500

    Week 1 Tasks

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+#print("Hello World!")
+#Greeting menu for database
+#theme: database on items in video games (weapons)
+
+print("Welcome to the Weapon Arsenal.")
+
+print("-Select-")
+print("1. Add Weapon")
+print("2. Remove Weapon")
+print("3. List Items")
+menu_choice = input("> ")
+
+if menu_choice == "A":
+    print("\nTest1")
+
